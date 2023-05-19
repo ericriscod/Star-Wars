f82b8f21b5aadeee8979f14419509739bf0d2622
Ericris Rossato <rossato.ep@gmail.com>
2023-05-18 21:00:29 -0300

    Tag button with class card

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-var cards = document.getElementsByClassName('link')
-var sound = document.getElementById('sabre')
 
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('mouseover',()=>{
-        sound.play()
-        console.log('passei aqui')
-      
-    })
+function mouveover() {
+    const cards = document.getElementsByClassName('link')
+    const sound = document.getElementById('sabre')
 
-    cards[i].addEventListener('mouseout', ()=>{
-        sound.pause()
-        sound.currentTime = 0
-        console.log('saindo')
-    })
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('mouseover', () => {
+            sound.play()
+            console.log('mouseover')
+
+        })
+        cards[i].addEventListener('mouseout', () => {
+            sound.pause()
+            sound.currentTime = 0
+            console.log('mouseout')
+        })
+    }
 }
+
+function volumeMusic(volume) {
+    const music = document.getElementById('music')
+    music.volume = volume
+}
+
+volumeMusic(0.4)
+mouveover()
