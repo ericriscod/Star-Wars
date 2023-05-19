c89866d9ffb16f1f287e82459505fd990f497ca3
Ericris Rossato <rossato.ep@gmail.com>
2023-05-18 22:38:29 -0300

    Main completed

--- js/main.js
+++ js/main.js
@@ -1,26 +1,25 @@
 
 function mouveover() {
     const cards = document.getElementsByClassName('link')
-    const sound = document.getElementById('sabre')
+    // const music = document.getElementById('sabre')
+
+    const music = new Audio
+    const url = "./assets/sounds/sabre.mp3"
+
+    music.src = url
 
     for (let i = 0; i < cards.length; i++) {
         cards[i].addEventListener('mouseover', () => {
-            sound.play()
+            music.play()
+            music.volume = 0.3
             console.log('mouseover')
-
         })
         cards[i].addEventListener('mouseout', () => {
-            sound.pause()
-            sound.currentTime = 0
+            music.pause()
+            music.currentTime = 0
             console.log('mouseout')
         })
     }
 }
 
-function volumeMusic(volume) {
-    const music = document.getElementById('music')
-    music.volume = volume
-}
-
-volumeMusic(0.4)
 mouveover()
